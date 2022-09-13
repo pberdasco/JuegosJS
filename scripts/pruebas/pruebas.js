18c2bed3b7591fb33d7b90fd7730d1df763066d2
Pablo Berdasco <pablo.berdasco@simplidata.com.ar>
2022-09-13 20:29:36 -0300

    pruebas varias

--- scripts/pruebas/pruebas.js
+++ scripts/pruebas/pruebas.js
@@ -9,6 +9,26 @@ const menuIndex = document.getElementsByTagName("ul");
 const nuevoBoton = document.createElement("li");
 nuevoBoton.innerHTML = `<button class="botonNav" type="button" onclick="CargarPagina('./testGrid.html')" id="btnAgregado">Boton Nuevo grid</button>`
 menuIndex[0].append(nuevoBoton);
+nuevoBoton.style.backgroundColor = "red";
+
+
+const ulTags = document.getElementsByTagName("ul");
+const boton = ulTags[0].children[3];
+boton.style.backgroundColor = "red";
+
+console.log("ul-lis", ulTags[0].children);
+console.log("li-3-children", ulTags[0].children[3]);
+console.log("li-3-chnode", ulTags[0].childNodes[3]);
+
+
+const h1Tag = document.getElementsByTagName("h1");
+h1Tag[0].addEventListener("click", (ev) => alert(`Presionaron click sobre el h1. ${ev.target}`));
+
+//h1Tag[0].onclick = function(){ClickSobreH1};
+
+console.log(h1Tag[0].innerText);
+h1Tag[0].innerText= `Nuevo 
+                     Titulo`;
 
 
 // Prueba de onwheel="MueveRueda(event)"
